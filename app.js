670328562c7e5a93c8daa83c047f6d7e580c7876
Ali Khalil <83224003+JanuMshkillz@users.noreply.github.com>
2021-12-15 16:34:59 +0500

    Final commit

--- app.js
+++ app.js
@@ -7,4 +7,22 @@ const about = document.querySelector(".about");
 // Grabbing articles
 const articles = document.querySelectorAll(".content");
 
-// Putting
+// Putting event listener
+about.addEventListener("click", (e) => {
+  // console.log(e.target.dataset.id);
+  const id = e.target.dataset.id;
+  if (id) {
+    // Removing active status from all buttons
+    btns.forEach(removeActive);
+    // Setting one button to active
+    e.target.classList.add("active");
+  }
+  // hide other articles
+  articles.forEach(removeActive);
+  const element = document.getElementById(id);
+  element.classList.add("active");
+});
+
+function removeActive(button) {
+  button.classList.remove("active");
+}
