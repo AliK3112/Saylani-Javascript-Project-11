9842664849ad2cd324a311f6e040da619ff3ac6b
Ali Khalil <83224003+JanuMshkillz@users.noreply.github.com>
2021-12-15 16:25:11 +0500

    HTML completed

---
+++ app.js
@@ -0,0 +1,10 @@
+// Grabbing all buttons
+const btns = document.querySelectorAll(".tab-btn");
+
+// Grabbing about section
+const about = document.querySelector(".about");
+
+// Grabbing articles
+const articles = document.querySelectorAll(".content");
+
+// Putting
